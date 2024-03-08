3eb2eee067dabc1e1ff2c5d71f3fb5515e11b43c
Leonov Egor <crozetle@gmail.com>
2024-03-08 17:23:37 +0400

    Добавьте файлы проекта.

---
+++ Fedotov-s-programm/1.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int age, answer;
+
+    printf("Введите год рождения:");
+    scanf("%d", &age);
+
+    answer = 2035 - age;
+    printf("В 2035 году вм будте %d лет", answer);
+
+    return(0);
+}
+
